244fb85265d93c187c1477f41831f1d4bd1cf541
bbasung1 <bbasung1@daum.net>
2022-07-04 22:01:48 +0900

    add station search

--- module.py
+++ module.py
@@ -1,9 +1,13 @@
 import urllib.request
 import xml.etree.ElementTree as et
+
+from scipy.fft import idct
 def getapi(url,param):
-    qu=['http://apis.data.go.kr/6410000/buslocationservice/getBusLocationList?serviceKey=PT8WA9S%2BrAN3hvm6e2CSDvOaiWJlRWpu%2BvQ7qNtKAylxcLJILSF%2FLQyGWfsCNvLmCGnzjCAmULm57lZ8ayGC9g%3D%3D&routeId=','http://apis.data.go.kr/6410000/busrouteservice/getBusRouteList?serviceKey=PT8WA9S%2BrAN3hvm6e2CSDvOaiWJlRWpu%2BvQ7qNtKAylxcLJILSF%2FLQyGWfsCNvLmCGnzjCAmULm57lZ8ayGC9g%3D%3D&keyword=']
-    #qu[0]:url에 해당하는 busid를 가진 버스의 위치를 알려줍니다
+    qu=['http://apis.data.go.kr/6410000/buslocationservice/getBusLocationList?serviceKey=PT8WA9S%2BrAN3hvm6e2CSDvOaiWJlRWpu%2BvQ7qNtKAylxcLJILSF%2FLQyGWfsCNvLmCGnzjCAmULm57lZ8ayGC9g%3D%3D&routeId=','http://apis.data.go.kr/6410000/busrouteservice/getBusRouteList?serviceKey=PT8WA9S%2BrAN3hvm6e2CSDvOaiWJlRWpu%2BvQ7qNtKAylxcLJILSF%2FLQyGWfsCNvLmCGnzjCAmULm57lZ8ayGC9g%3D%3D&keyword=',
+    'https://apis.data.go.kr/6410000/busrouteservice/getBusRouteStationList?serviceKey=PT8WA9S%2BrAN3hvm6e2CSDvOaiWJlRWpu%2BvQ7qNtKAylxcLJILSF%2FLQyGWfsCNvLmCGnzjCAmULm57lZ8ayGC9g%3D%3D&routeId=']
+    #qu[0]:url에 해당하는 routeid를 가진 버스의 위치를 알려줍니다
     #qu[1]:url에 해당하는 번호를 가진 버스들의 목록을 가지고 옵니다.
+    #qu[2]:url에 해당하는 routeid를 가진 버스의 노선 목록을 가지고 옵니다.
     #테스트를 할떄 사용할 버스 노선 목록
     #7770(routeid:233000031): 24시간 운영하는 버스임으로 정상 테스트 하기에 좋음
     #8147(routeid:241000820): 오후 10시 30분 경이면 운행이 종료됨으로 잘못된 정보 수신시 테스트 할 떄 좋음
@@ -28,3 +32,14 @@ def getapi(url,param):
     # 3. routeName:노선번호
     # 4. routeTypeCd:노선종류 번호
     # 5. routeTypeName: 노선 종류
+    # qu[2]
+    # 0. centerYn:중앙차로 여부
+    # 1. districtCd: 구역 번호
+    # 2. moblieNo: 정류장 번호
+    # 3. regionName: 지역 이름
+    # 4. stationId: api에서 사용되는 정류장idct
+    # 5. stationName: 정류장 이름
+    # 6. x: 정류장의 x좌표
+    # 7. y: 정류장의 y좌표
+    # 8. stationSeq: 정류장 순번
+    # 9. turnYn: 회자지점 여부
